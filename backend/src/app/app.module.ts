1341eb456784b5d7d2b036ca5a366ce3af6b0c06
vitororb <vitororesendeb@gmail.com>
2026-03-10 23:12:50 -0400

    feat: configura configurações globais para conexão com banco de dados

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SuitesModule } from '../suites/suites.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    }),
+    SuitesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
