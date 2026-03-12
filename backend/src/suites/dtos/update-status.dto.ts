5f6ee68d721f317ca4a316fd01ed4df9ec8bb3cc
vitororb <vitororesendeb@gmail.com>
2026-03-12 00:00:24 -0400

    feat: adiciona DTO UpdateStatusDto e validações com class-validator

---
+++ backend/src/suites/dtos/update-status.dto.ts
@@ -0,0 +1,8 @@
+import { IsEnum, IsNotEmpty } from 'class-validator';
+import { SuiteStatus } from '../enum/suite-status.enum';
+
+export class UpdateStatusDto {
+  @IsEnum(SuiteStatus)
+  @IsNotEmpty()
+  status: SuiteStatus;
+}
