04d3f75ac4f7e4278ad2b3c18f3c1de8bb31c397
vitororb <vitororesendeb@gmail.com>
2026-03-20 10:31:14 -0400

    feat: adiciona componente Providers para gerenciamento de estado com React Query

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from "@/lib/providers";
 import { Footer } from "@/shared/ui/Footer";
 import { Navbar } from "@/shared/ui/Navbar";
 import { Sidebar } from "@/shared/ui/Sidebar";
@@ -30,19 +31,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex flex-col w-screen h-screen">
-          <Navbar />
+        <Providers>
+          <div className="flex flex-col w-screen h-screen">
+            <Navbar />
 
-          <div className="flex flex-1 pr-2">
-            <Sidebar />
+            <div className="flex flex-1 pr-2">
+              <Sidebar />
 
-            <main className="flex-1 bg-[#191A1C] rounded-xl p-4">
-              {children}
-            </main>
+              <main className="flex-1 bg-[#191A1C] rounded-xl p-4">
+                {children}
+              </main>
+            </div>
+
+            <Footer />
           </div>
-
-          <Footer />
-        </div>
+        </Providers>
       </body>
     </html>
   );
