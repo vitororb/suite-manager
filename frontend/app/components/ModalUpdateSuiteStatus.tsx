bf17be48bf762a117468620f91420a8530a3fa3b
vitororb <vitororesendeb@gmail.com>
2026-03-25 23:11:10 -0400

    feat: melhora componentes: Modal, SuiteCard e ModalUpdateSuiteStatus

--- frontend/app/components/ModalUpdateSuiteStatus.tsx
+++ frontend/app/components/ModalUpdateSuiteStatus.tsx
@@ -11,11 +11,59 @@ import {
   Trash2,
   Wrench,
 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StatusButton } from "./StatusButton";
 
+const statusOptions = [
+  {
+    label: SuiteStatus.livre,
+    icon: <Key size={16} />,
+    color: "text-emerald-600",
+  },
+  {
+    label: SuiteStatus.bloqueado,
+    icon: <Ban size={16} />,
+    color: "text-gray-500",
+  },
+  {
+    label: SuiteStatus.conferencia,
+    icon: <ScanEye size={16} />,
+    color: "text-yellow-600",
+  },
+  {
+    label: SuiteStatus.faxina,
+    icon: <Bubbles size={16} />,
+    color: "text-purple-800",
+  },
+  {
+    label: SuiteStatus.fechamento,
+    icon: <Scroll size={16} />,
+    color: "text-amber-200",
+  },
+  {
+    label: SuiteStatus.limpeza,
+    icon: <BrushCleaning size={16} />,
+    color: "text-blue-600",
+  },
+  {
+    label: SuiteStatus.locado,
+    icon: <LockKeyhole size={16} />,
+    color: "text-red-800",
+  },
+  {
+    label: SuiteStatus.manutencao,
+    icon: <Wrench size={16} />,
+    color: "text-gray-600",
+  },
+  {
+    label: SuiteStatus.sujo,
+    icon: <Trash2 size={16} />,
+    color: "text-amber-900",
+  },
+];
+
 type ModalUpdateSuiteStatusProps = {
-  open?: boolean;
+  isOpen?: boolean;
   suite: Suite;
   isUpdating?: boolean;
   onClose?: () => void;
@@ -23,66 +71,24 @@ type ModalUpdateSuiteStatusProps = {
 };
 
 export const ModalUpdateSuiteStatus = ({
-  open,
+  isOpen,
   suite,
   isUpdating,
   onClose,
   onUpdate,
 }: ModalUpdateSuiteStatusProps) => {
-  const [selectedStatus, setSelectedStatus] = useState<SuiteStatus>(
-    suite?.status,
+  const [selectedStatus, setSelectedStatus] = useState<SuiteStatus | null>(
+    null,
   );
-  const statusOptions = [
-    {
-      label: SuiteStatus.livre,
-      icon: <Key size={16} />,
-      color: "text-emerald-600",
-    },
-    {
-      label: SuiteStatus.bloqueado,
-      icon: <Ban size={16} />,
-      color: "text-gray-500",
-    },
-    {
-      label: SuiteStatus.conferencia,
-      icon: <ScanEye size={16} />,
-      color: "text-yellow-600",
-    },
-    {
-      label: SuiteStatus.faxina,
-      icon: <Bubbles size={16} />,
-      color: "text-purple-800",
-    },
-    {
-      label: SuiteStatus.fechamento,
-      icon: <Scroll size={16} />,
-      color: "text-amber-200",
-    },
-    {
-      label: SuiteStatus.limpeza,
-      icon: <BrushCleaning size={16} />,
-      color: "text-blue-600",
-    },
-    {
-      label: SuiteStatus.locado,
-      icon: <LockKeyhole size={16} />,
-      color: "text-red-800",
-    },
-    {
-      label: SuiteStatus.manutencao,
-      icon: <Wrench size={16} />,
-      color: "text-gray-600",
-    },
-    {
-      label: SuiteStatus.sujo,
-      icon: <Trash2 size={16} />,
-      color: "text-amber-900",
-    },
-  ];
 
   const handleSelectStatus = (status: SuiteStatus) => {
     setSelectedStatus(status);
   };
+
+  useEffect(() => {
+    if (!isOpen) return;
+    setSelectedStatus(null);
+  }, [isOpen, suite?.id]);
 
   return (
     <Modal
@@ -90,21 +96,23 @@ export const ModalUpdateSuiteStatus = ({
       title="Alterar Status"
       subtitle={`Suíte ${suite?.number}`}
       onClose={onClose}
-      open={open}
+      isOpen={isOpen}
     >
       <div className="flex flex-col gap-2 h-80 overflow-auto">
-        {statusOptions.map((option) => {
-          return (
-            <StatusButton
-              key={option.label}
-              icon={option.icon}
-              color={option.color}
-              label={option.label}
-              onClick={handleSelectStatus}
-              isSelected={selectedStatus === option.label}
-            />
-          );
-        })}
+        {statusOptions
+          .filter((option) => option.label !== suite?.status)
+          .map((option) => {
+            return (
+              <StatusButton
+                key={option.label}
+                icon={option.icon}
+                color={option.color}
+                label={option.label}
+                onClick={handleSelectStatus}
+                isSelected={selectedStatus === option.label}
+              />
+            );
+          })}
       </div>
 
       <div className="flex w-full justify-end items-center gap-2">
@@ -115,9 +123,9 @@ export const ModalUpdateSuiteStatus = ({
           Cancelar
         </button>
         <button
-          disabled={isUpdating}
-          onClick={() => onUpdate(selectedStatus)}
-          className="flex gap-2 items-center hover:cursor-pointer text-xs px-3 py-1.5 rounded-lg bg-white text-black hover:shadow-lg disabled:opacity-50 disabled:hover:shadow-none transition-shadow"
+          disabled={isUpdating || !selectedStatus}
+          onClick={() => onUpdate(selectedStatus!)}
+          className="flex gap-2 items-center hover:cursor-pointer text-xs px-3 py-1.5 rounded-lg bg-white text-black hover:shadow-lg disabled:opacity-50 disabled:hover:shadow-none disabled:cursor-not-allowed transition-shadow"
         >
           {isUpdating ? "Atualizando..." : "Atualizar"}
         </button>
--- frontend/app/components/SuiteCard.tsx
+++ frontend/app/components/SuiteCard.tsx
@@ -7,7 +7,6 @@ import {
   Bubbles,
   Clock4,
   Key,
-  LoaderCircle,
   LockKeyhole,
   PlusCircle,
   ScanEye,
@@ -20,65 +19,74 @@ import { SuiteInfoRow } from "./SuiteInfoRow";
 
 type SuiteCardProps = {
   suite: Suite;
-  onOpen: (suite: Suite) => void;
+  onOpenStatusModal: (suite: Suite) => void;
+  onAddAlert: (suite: Suite) => void;
 };
 
-export const SuiteCard = ({ suite, onOpen }: SuiteCardProps) => {
-  const getStatusStyle = () => {
-    switch (suite.status) {
-      case SuiteStatus.livre:
-        return {
-          color: "bg-emerald-600 text-emerald-600",
-          icon: <Key size={16} />,
-        };
-      case SuiteStatus.bloqueado:
-        return {
-          color: "bg-gray-500 text-gray-500",
-          icon: <Ban size={16} />,
-        };
-      case SuiteStatus.conferencia:
-        return {
-          color: "bg-yellow-600 text-yellow-600",
-          icon: <ScanEye size={16} />,
-        };
-      case SuiteStatus.faxina:
-        return {
-          color: "bg-purple-800 text-purple-800",
-          icon: <Bubbles size={16} />,
-        };
-      case SuiteStatus.fechamento:
-        return {
-          color: "bg-amber-200 text-amber-200",
-          icon: <Scroll size={16} />,
-        };
-      case SuiteStatus.limpeza:
-        return {
-          color: "bg-blue-600 text-blue-600",
-          icon: <BrushCleaning size={16} />,
-        };
-      case SuiteStatus.locado:
-        return {
-          color: "bg-red-800 text-red-800",
-          icon: <LockKeyhole size={16} />,
-        };
-      case SuiteStatus.manutencao:
-        return {
-          color: "bg-gray-600 text-gray-600",
-          icon: <Wrench size={16} />,
-        };
-      case SuiteStatus.sujo:
-        return {
-          color: "bg-amber-900 text-amber-900",
-          icon: <Trash2 size={16} />,
-        };
-    }
+const statusMap = {
+  [SuiteStatus.livre]: {
+    label: SuiteStatus.livre,
+    color: "bg-emerald-600 text-emerald-600",
+    icon: <Key size={16} />,
+  },
+  [SuiteStatus.bloqueado]: {
+    label: SuiteStatus.bloqueado,
+    color: "bg-gray-500 text-gray-500",
+    icon: <Ban size={16} />,
+  },
+  [SuiteStatus.conferencia]: {
+    label: SuiteStatus.conferencia,
+    color: "bg-yellow-600 text-yellow-600",
+    icon: <ScanEye size={16} />,
+  },
+  [SuiteStatus.faxina]: {
+    label: SuiteStatus.faxina,
+    color: "bg-purple-800 text-purple-800",
+    icon: <Bubbles size={16} />,
+  },
+  [SuiteStatus.fechamento]: {
+    label: SuiteStatus.fechamento,
+    color: "bg-amber-200 text-amber-200",
+    icon: <Scroll size={16} />,
+  },
+  [SuiteStatus.limpeza]: {
+    label: SuiteStatus.limpeza,
+    color: "bg-blue-600 text-blue-600",
+    icon: <BrushCleaning size={16} />,
+  },
+  [SuiteStatus.locado]: {
+    label: SuiteStatus.locado,
+    color: "bg-red-800 text-red-800",
+    icon: <LockKeyhole size={16} />,
+  },
+  [SuiteStatus.manutencao]: {
+    label: SuiteStatus.manutencao,
+    color: "bg-gray-600 text-gray-600",
+    icon: <Wrench size={16} />,
+  },
+  [SuiteStatus.sujo]: {
+    label: SuiteStatus.sujo,
+    color: "bg-amber-900 text-amber-900",
+    icon: <Trash2 size={16} />,
+  },
+};
+
+export const SuiteCard = ({
+  suite,
+  onOpenStatusModal,
+  onAddAlert,
+}: SuiteCardProps) => {
+  const statusStyle = statusMap[suite.status] ?? {
+    label: "Desconhecido",
+    color: "bg-gray-400 text-gray-400",
+    icon: <AlertTriangle size={16} />,
   };
 
   return (
     <div className="flex flex-col w-60 h-fit rounded-lg shadow-sm overflow-hidden">
       {/* Header */}
       <div
-        className={`flex items-start justify-between w-full p-4 ${getStatusStyle().color}`}
+        className={`flex items-start justify-between w-full p-4 ${statusStyle.color}`}
       >
         {/* Number and Category */}
         <div className="flex flex-col gap-2 text-white">
@@ -90,15 +98,11 @@ export const SuiteCard = ({ suite, onOpen }: SuiteCardProps) => {
 
         {/* Status */}
         <button
-          onClick={() => onOpen(suite)}
+          onClick={() => onOpenStatusModal(suite)}
           className="flex items-center gap-2 px-3 py-1.5 rounded-full bg-white text-sm font-medium whitespace-nowrap hover:cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
         >
-          {false ? (
-            <LoaderCircle size={16} className="animate-spin" />
-          ) : (
-            getStatusStyle().icon
-          )}
-          <span>{false ? "Atualizando..." : suite.status}</span>
+          {statusStyle.icon}
+          {suite.status}
         </button>
       </div>
 
@@ -107,19 +111,22 @@ export const SuiteCard = ({ suite, onOpen }: SuiteCardProps) => {
         <SuiteInfoRow
           icon={<Clock4 size={16} />}
           label="Último checkout"
-          time={format(suite.checkOut, "HH:mm")}
+          time={suite.checkOut ? format(suite.checkOut, "HH:mm") : "--:--"}
         />
 
         <SuiteInfoRow
           icon={<Clock4 size={16} />}
           label="Período"
-          time={format(suite.checkIn, "HH:mm")}
+          time={suite.checkIn ? format(suite.checkIn, "HH:mm") : "--:--"}
         />
 
         <div className="w-full h-px bg-gray-400" />
 
         {!suite.alert && (
-          <button className="flex text-gray-400 items-center gap-1 cursor-pointer whitespace-nowrap w-fit h-8 hover:text-gray-200 hover:underline">
+          <button
+            onClick={() => onAddAlert(suite)}
+            className="flex text-gray-400 items-center gap-1 cursor-pointer whitespace-nowrap w-full justify-center border rounded-md h-8 hover:text-gray-200 border-dashed"
+          >
             <PlusCircle size={16} className="shrink-0" />
             <p className="text-xs font-medium">Adicionar alerta</p>
           </button>
