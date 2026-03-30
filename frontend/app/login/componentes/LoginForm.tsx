28c774f543583273c4cc63f2779a559852e66351
vitororb <vitororesendeb@gmail.com>
2026-03-29 23:24:32 -0400

    feat: implementa componente LoginForm inicial para a tela de login

---
+++ frontend/app/login/componentes/LoginForm.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/shared/ui/Button";
+import { Input } from "@/shared/ui/Input";
+import { EyeOff, Lock, LogIn, Mail } from "lucide-react";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export const LoginForm = () => {
+  return (
+    <div className="flex flex-col items-center rounded-2xl bg-[#171717] p-6 gap-7 border-t-4 border-b-4 border-neutral-600/20 w-sm">
+      <div className="flex items-center justify-center p-2 rounded-2xl shadow-lg bg-gray-600/10">
+        <Image src="/logo.png" alt="Logo" width={40} height={40} />
+      </div>
+
+      <div className="flex flex-col gap-1 items-center text-center w-full">
+        <h2 className="text-xl font-medium">Bem-vindo de volta!</h2>
+        <span className="text-sm text-neutral-500">
+          Faça login para acessar sua conta.
+        </span>
+      </div>
+
+      <div className="flex flex-col gap-2 w-full">
+        <Input
+          leftIcon={<Mail size={16} />}
+          label="Email"
+          placeholder="Digite seu email"
+        />
+        <Input
+          leftIcon={<Lock size={16} />}
+          label="Senha"
+          placeholder="Digite sua senha"
+          rightIcon={<EyeOff size={16} />}
+        />
+      </div>
+
+      <div className="flex w-full justify-end">
+        <Button onClick={() => redirect("/")}>
+          <div className="flex items-center gap-1">
+            Entrar
+            <LogIn size={16} />
+          </div>
+        </Button>
+      </div>
+    </div>
+  );
+};
