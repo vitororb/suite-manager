167477f2181c83d347937ee8333a2a559301da95
vitororb <vitororesendeb@gmail.com>
2026-04-06 10:39:21 -0400

    fix: corrige caminho de importação do StocksModule e remove baseUrl do tsconfig

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { StocksModule } from 'src/stocks/stocks.module';
+import { StocksModule } from '../stocks/stocks.module';
 import { SuitesModule } from '../suites/suites.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
